a304b2bfe6fe70a10cd26d717f6b85bcb098ba2c
Malvadoficial <malvadoficial@gmail.com>
2026-02-19 04:58:08 +0100

    Initial Commit

---
+++ UtilClock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UtilClock
+//
+//  Created by José Manuel Rives on 19/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UtilClock/UtilClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtilClockApp.swift
+//  UtilClock
+//
+//  Created by José Manuel Rives on 19/2/26.
+//
+
+import SwiftUI
+
+@main
+struct UtilClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
